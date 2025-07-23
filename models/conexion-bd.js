e5e41fdd4589c866aeaf66c9bf59cdc38426ea22
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-07-23 12:00:07 -0500

    23-07-2025

--- models/conexion-bd.js
+++ models/conexion-bd.js
@@ -1,5 +1,6 @@
 /*
     Author: German Valencia
+    Actualización: John Castañeda
 */
 const { DataTypes } = require('sequelize');
 
@@ -18,6 +19,11 @@ module.exports = (sequelize) => {
         suscriptorId: {
             type: DataTypes.INTEGER,
             default: 0,
+            allowNull: false
+        },
+        paqueteId:{
+            type: DataTypes.INTEGER,
+            default:0,
             allowNull: false
         },
         nombreConexion: {
