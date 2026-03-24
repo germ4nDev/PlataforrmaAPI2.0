04a6a516e0c2134cc2b8ea8b25ea87dc8709f9da
JohnCastañeda <jhoncastaneda128@gmail.com>
2026-03-24 18:31:30 -0500

    ajustes paquetes
    20260324

--- controllers/items-paquete.js
+++ controllers/items-paquete.js
@@ -6,143 +6,177 @@ const sequelize = require('../database/connection');
 const PTLItemsPaquete = require('../models/item-paquete')(sequelize);
 const { io } = require('../index');
 
-const getItemsPaquete = async (req, res) => {
-  try {
-    const itemsPaquete = await PTLItemsPaquete.findAll();
-    return res.status(201).json({
-      ok: true,
-      itemsPaquete: itemsPaquete,
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al obtener ItemsPaquete, ' + err });
-  }
-};
-
-const getItemsPaqueteById = async (req, res) => {
-  try {
-    const codigoItem = req.params.id;
-    const itemsPaquete = await PTLItemsPaquete.findOne({
-      where: { codigoItem },
-    });
-    if (!itemsPaquete) {
-      return res.status(404).json({
-        ok: false,
-        msg: "No existe un itemsPaquete con ese id",
-      });
+const getItemsPaquete = async(req, res) => {
+    try {
+        const itemsPaquete = await PTLItemsPaquete.findAll();
+        return res.status(201).json({
+            ok: true,
+            itemsPaquete: itemsPaquete,
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Error al obtener ItemsPaquete, ' + err });
     }
-    return res.status(201).json({
-      ok: true,
-      itemsPaquete: itemsPaquete,
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al obtener el paquete' });
-  }
 };
 
-const getItemsPaqueteByCode = async (req, res) => {
-  try {
-    const codego = req.params.code;
-    const itemsPaquete = await PTLItemsPaquete.findAll({
-      where: {
-        codigoPaquete: codego,
-      },
-    });
-    if (!itemsPaquete) {
-      return res.status(404).json({
-        ok: false,
-        msg: "No existe un itemsPaquete por el codigo",
-      });
+const getItemsPaqueteById = async(req, res) => {
+    try {
+        const codigoItem = req.params.id;
+        const itemsPaquete = await PTLItemsPaquete.findOne({
+            where: { codigoItem },
+        });
+        if (!itemsPaquete) {
+            return res.status(404).json({
+                ok: false,
+                msg: "No existe un itemsPaquete con ese id",
+            });
+        }
+        return res.status(201).json({
+            ok: true,
+            itemsPaquete: itemsPaquete,
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Error al obtener el paquete' });
     }
-    return res.status(201).json({
-      ok: true,
-      itemsPaquete: itemsPaquete,
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Error al obtener itemsPaquete" });
-  }
 };
 
-const createItemsPaquete = async (req, res = response) => {
-  const { ...newRegistro } = req.body;
-  try {
-    const nuevo = await PTLItemsPaquete.create(newRegistro);
-    io.emit('items-paquete-actualizados', {
-      action: 'create',
-      msg: `Item creado: ${nuevo.nombreItem}`
-    });
-    return res.status(201).json({
-      ok: true,
-      estado: estadoDB
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al crear el itemsPaquete' });
-  }
-};
-
-const updateItemsPaquete = async (req, res = response) => {
-  const { codigoItem, ...data } = req.body;
-  try {
-    const itemsPaqueteOg = await PTLItemsPaquete.findOne({
-      where: { codigoItem },
-    });
-    if (!itemsPaqueteOg) {
-      return res.status(404).json({
-        ok: false,
-        msg: "No existe un paquete con ese id",
-      });
+const getItemsPaqueteByCode = async(req, res) => {
+    try {
+        const codego = req.params.code;
+        const itemsPaquete = await PTLItemsPaquete.findAll({
+            where: {
+                codigoPaquete: codego,
+            },
+        });
+        if (!itemsPaquete) {
+            return res.status(404).json({
+                ok: false,
+                msg: "No existe un itemsPaquete por el codigo",
+            });
+        }
+        return res.status(201).json({
+            ok: true,
+            itemsPaquete: itemsPaquete,
+        });
+    } catch (err) {
+        res.status(500).json({ error: "Error al obtener itemsPaquete" });
     }
-    await PTLItemsPaquete.update(data, {
-      where: { codigoItem },
-    });
-    const itemsPaqueteActualizado = await PTLItemsPaquete.findOne({
-      where: { codigoItem },
-    });
-    io.emit('items-paquete-actualizados', {
-      action: 'update',
-      msg: `Item actualizado: ${itemsPaqueteActualizado.nombreItem}`
-    });
-    return res.status(201).json({
-      ok: true,
-      itemsPaquete: itemsPaqueteActualizado,
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al actualizar el paquete' });
-  }
 };
 
-const deleteItemsPaquete = async (req, res = response) => {
-  try {
-    const codigoItem = req.params.id;
-    const itemsPaquete = await PTLItemsPaquete.findOne({
-      where: { codigoItem },
-    });
-    if (!itemsPaquete) {
-      return res.status(404).json({
-        ok: false,
-        msg: "No existe un paquete con ese id",
-      });
+const createItemsPaquete = async(req, res = response) => {
+    const {...newRegistro } = req.body;
+    console.log('crear registro', newRegistro);
+    try {
+        const existente = await PTLItemsPaquete.findOne({
+            where: { codigoItem: newRegistro.codigoItem }
+        });
+
+        if (existente) {
+            return res.status(400).json({
+                ok: false,
+                msg: `El código ${newRegistro.codigoItem} ya está registrado`
+            });
+        }
+
+        const nuevo = await PTLItemsPaquete.create(newRegistro);
+
+        if (typeof io !== 'undefined') {
+            io.emit('items-paquete-actualizados', {
+                action: 'create',
+                msg: `Items Paquete creado: ${nuevo.codigoItem}`
+            });
+        } else {
+            console.warn('Advertencia: Socket.io (io) no está definido, no se emitió el evento.');
+        }
+
+        return res.status(201).json({
+            ok: true,
+            paquete: nuevo,
+        });
+
+    } catch (err) {
+        console.error('--- ERROR EN CREATE PAQUETE ---');
+        console.error(err);
+
+        if (err.name === 'SequelizeValidationError') {
+            return res.status(400).json({
+                ok: false,
+                error: "Faltan campos obligatorios",
+                detalles: err.errors.map(e => e.message)
+            });
+        }
+
+        res.status(500).json({
+            ok: false,
+            error: "Error interno en el servidor",
+            msg: err.message
+        });
     }
-    const itemsPaqueteEliminado = await PTLItemsPaquete.destroy({
-      where: { codigoItem },
-    });
-    io.emit('items-paquete-actualizados', {
-      action: 'delete',
-      msg: `Item eliminado: ${itemsPaquete.nombreItem}`
-    });
-    return res.status(201).json({
-      ok: true,
-      itemsPaquete: itemsPaqueteEliminado
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al eliminar itemsPaquete' });
-  }
+};
+
+const updateItemsPaquete = async(req, res = response) => {
+    const { codigoItem, ...data } = req.body;
+    try {
+        const itemsPaqueteOg = await PTLItemsPaquete.findOne({
+            where: { codigoItem },
+        });
+        if (!itemsPaqueteOg) {
+            return res.status(404).json({
+                ok: false,
+                msg: "No existe un paquete con ese id",
+            });
+        }
+        await PTLItemsPaquete.update(data, {
+            where: { codigoItem },
+        });
+        const itemsPaqueteActualizado = await PTLItemsPaquete.findOne({
+            where: { codigoItem },
+        });
+        io.emit('items-paquete-actualizados', {
+            action: 'update',
+            msg: `Item actualizado: ${itemsPaqueteActualizado.nombreItem}`
+        });
+        return res.status(201).json({
+            ok: true,
+            itemsPaquete: itemsPaqueteActualizado,
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Error al actualizar el paquete' });
+    }
+};
+
+const deleteItemsPaquete = async(req, res = response) => {
+    try {
+        const codigoItem = req.params.id;
+        const itemsPaquete = await PTLItemsPaquete.findOne({
+            where: { codigoItem },
+        });
+        if (!itemsPaquete) {
+            return res.status(404).json({
+                ok: false,
+                msg: "No existe un paquete con ese id",
+            });
+        }
+        const itemsPaqueteEliminado = await PTLItemsPaquete.destroy({
+            where: { codigoItem },
+        });
+        io.emit('items-paquete-actualizados', {
+            action: 'delete',
+            msg: `Item eliminado: ${itemsPaquete.nombreItem}`
+        });
+        return res.status(201).json({
+            ok: true,
+            itemsPaquete: itemsPaqueteEliminado
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Error al eliminar itemsPaquete' });
+    }
 };
 
 module.exports = {
-  getItemsPaquete,
-  getItemsPaqueteById,
-  getItemsPaqueteByCode,
-  createItemsPaquete,
-  updateItemsPaquete,
-  deleteItemsPaquete,
+    getItemsPaquete,
+    getItemsPaqueteById,
+    getItemsPaqueteByCode,
+    createItemsPaquete,
+    updateItemsPaquete,
+    deleteItemsPaquete,
 };
--- models/item-paquete.js
+++ models/item-paquete.js
@@ -5,71 +5,75 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  return sequelize.define('PTLItemsPaquete', {
-    itemId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    codigoItem: {
-      type: DataTypes.STRING(200),
-      primaryKey: true,
-      allowNull: false
-    },
-    codigoPaquete: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    codigoValor: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    nombreItem: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    descripcionItem: {
-      type: DataTypes.STRING(4000),
-      allowNull: false
-    },
-    cantidad: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    valorUnitario: {
-      type: DataTypes.DECIMAL(18, 2),
-      allowNull: false
-    },
-    valorTotal: {
-      type: DataTypes.DECIMAL(18, 2),
-      allowNull: false
-    },
-    valoresAdicionales: {
-      type: DataTypes.DECIMAL(18, 2),
-      allowNull: false
-    },
-    estadoItem: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    },
-    // AUDITORIA ------------
-    codigoUsuarioCreacion: {
-      type: DataTypes.STRING(200),
-      allowNull: true
-    },
-    fechaCreacion: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    },
-    codigoUsuarioModificacion: {
-      type: DataTypes.STRING(200),
-      allowNull: true
-    },
-    fechaModificacion: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    }
-  }, {
-    tableName: 'PTLItemsPaquete',
-    timestamps: false
-  });
+    return sequelize.define('PTLItemsPaquete', {
+        itemId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true
+        },
+        codigoItem: {
+            type: DataTypes.STRING(200),
+            primaryKey: true,
+            allowNull: false
+        },
+        codigoPaquete: {
+            type: DataTypes.STRING(200),
+            allowNull: false
+        },
+        codigoValor: {
+            type: DataTypes.STRING(200),
+            allowNull: false
+        },
+        tipoValorId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        nombreItem: {
+            type: DataTypes.STRING(200),
+            allowNull: false
+        },
+        descripcionItem: {
+            type: DataTypes.STRING(4000),
+            allowNull: false
+        },
+        cantidad: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        valorUnitario: {
+            type: DataTypes.DECIMAL(18, 2),
+            allowNull: false
+        },
+        valorTotal: {
+            type: DataTypes.DECIMAL(18, 2),
+            allowNull: false
+        },
+        valoresAdicionales: {
+            type: DataTypes.DECIMAL(18, 2),
+            allowNull: false
+        },
+        estadoItem: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        // AUDITORIA ------------
+        codigoUsuarioCreacion: {
+            type: DataTypes.STRING(200),
+            allowNull: true
+        },
+        fechaCreacion: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        },
+        codigoUsuarioModificacion: {
+            type: DataTypes.STRING(200),
+            allowNull: true
+        },
+        fechaModificacion: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        }
+    }, {
+        tableName: 'PTLItemsPaquete',
+        timestamps: false
+    });
 };
