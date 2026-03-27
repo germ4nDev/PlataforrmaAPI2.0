3beb0941c52be69e6555799715f5b0529c34f13b
Juan Camilo Valencia <jcvalenciaortiz@gmail.com>
2026-03-27 17:07:02 -0500

    ajustes
    20260327

--- controllers/suscriptores.js
+++ controllers/suscriptores.js
@@ -9,20 +9,20 @@ const bcrypt = require("bcryptjs");
 const PTLSuscriptores = require('../models/suscriptor')(sequelize);
 const PTLUsuarios = require('../models/usuario')(sequelize);
 const PTLUsuariosSC = require('../models/usuario-sc')(sequelize);
-// import { v4 as uuidv4 } from 'uuid';
-const { io } = require('../index');
+// import { v4 as uuidv4 } from "uuid";
+const { io } = require("../index");
 
 const getSuscriptores = async (req, res) => {
   try {
-    console.log('aca');
+    console.log("aca");
     const suscriptores = await PTLSuscriptores.findAll();
     // console.log('todos los suscriptores', suscriptores);
     return res.status(201).json({
       ok: true,
-      suscriptores: suscriptores
+      suscriptores: suscriptores,
     });
   } catch (err) {
-    res.status(500).json({ error: 'Error al obtener suscriptores', err });
+    res.status(500).json({ error: "Error al obtener suscriptores", err });
   }
 };
 
@@ -51,20 +51,20 @@ const getSuscriptoresById = async (req, res) => {
 
 const createSuscriptor = async (req, res = response) => {
   const { ...newRegistro } = req.body;
-  console.log('newRegistro', newRegistro);
+  console.log("newRegistro", newRegistro);
   try {
     const existeNombre = await PTLSuscriptores.findOne({
-      where: { nombreSuscriptor: newRegistro.nombreSuscriptor }
+      where: { nombreSuscriptor: newRegistro.nombreSuscriptor },
     });
     if (existeNombre) {
       return res.status(400).json({
         ok: false,
-        msg: 'Ya existe un suscriptor con ese nombre'
+        msg: "Ya existe un suscriptor con ese nombre",
       });
     }
     const fechaActual = new Date();
     const administrador = {
-      codigoUsuario: uuidv4(),
+      codigoUsuario: "",
       identificacionUsuario: newRegistro.identificacionSuscriptor,
       nombreUsuario: newRegistro.nombreSuscriptor,
       correoUsuario: newRegistro.correoSuscriptor,
@@ -75,35 +75,35 @@ const createSuscriptor = async (req, res = response) => {
       usuarioAdministrador: true,
       estadoUsuario: true,
       codigoUsuarioCreacion: newRegistro.codigoSusucriptor,
-      fechaCreacion: fechaActual.toISOString()
-    }
-    const usuarioDB = await PTLUsuarios.create(administrador);
-    newRegistro.codigoAdministrador = administrador.codigoUsuario;
-    const suscriptorDB = await PTLSuscriptores.create(newRegistro);
-    const usuarioSuscriptor = {
-      codigoUsuarioSC: uuidv4(),
-      codigoUsuario: usuarioDB.codigoUsuario,
-      codigoSuscriptor: suscriptorDB.codigoSusucriptor,
-      estadoUsuarioSC: true,
-      codigoUsuarioCreacion: suscriptorDB.codigoSusucriptor,
-      fechaCreacion: fechaActual.toISOString()
-    }
-    const usuarioSCDB = await PTLUsuariosSC.create(usuarioSuscriptor);
-    io.emit('suscriptores-actualizados', {
-      action: 'create',
-      msg: `Suscriptor creada: ${suscriptorDB.nombreSuscriptor}`
+      fechaCreacion: fechaActual.toISOString(),
+    };
+    // const usuarioDB = await PTLUsuarios.create(administrador);
+    // newRegistro.codigoAdministrador = administrador.codigoUsuario;
+    // const suscriptorDB = await PTLSuscriptores.create(newRegistro);
+    // const usuarioSuscriptor = {
+    //   codigoUsuarioSC: uuidv4(),
+    //   codigoUsuario: usuarioDB.codigoUsuario,
+    //   codigoSuscriptor: suscriptorDB.codigoSusucriptor,
+    //   estadoUsuarioSC: true,
+    //   codigoUsuarioCreacion: suscriptorDB.codigoSusucriptor,
+    //   fechaCreacion: fechaActual.toISOString()
+    // }
+    // const usuarioSCDB = await PTLUsuariosSC.create(usuarioSuscriptor);
+    io.emit("suscriptores-actualizados", {
+      action: "create",
+      msg: `Suscriptor creada: ${suscriptorDB.nombreSuscriptor}`,
     });
     return res.status(201).json({
       ok: true,
       suscriptor: suscriptorDB,
       usuario: usuarioDB,
-      usuarioSC: usuarioSCDB
+      usuarioSC: usuarioSCDB,
     });
   } catch (err) {
     console.error(err);
     return res.status(500).json({
       ok: false,
-      error: 'Error al crear el suscriptor'
+      error: "Error al crear el suscriptor",
     });
   }
 };
